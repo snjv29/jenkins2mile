12386deb05f4e69d39c6c0ea1f4242c5d4dc04b0
snjv29 <snjvag29@gmail.com>
2024-06-05 15:50:44 +0530

    Initial commit

---
+++ src/main/java/NewController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NewController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Jenkins!";
+    }
+}
+
---
+++ src/main/java/com/example/dockermile4/dockermilestone4/Dockermilestone4Application.java
@@ -0,0 +1,13 @@
+package com.example.dockermile4.dockermilestone4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dockermilestone4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dockermilestone4Application.class, args);
+	}
+
+}
